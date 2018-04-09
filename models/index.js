0db7230b2df7d94f35161cef4b85d344da475438
faheemsidd15 <blazefs@gmail.com>
2018-04-08 20:55:43 -0400

    models

---
+++ models/index.js
@@ -0,0 +1,21 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+
+const models = {
+
+   user: sequelize.import(path.join(__dirname, file));
+};
+
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+export default 
